d1efa216b7e6918cfbef9da8d92f78601d4601e0
Jerome Bernard <jerome.bernard@elastic-grid.com>
2011-04-02 00:13:05 +0200

    Regenerated the documentation.

--- snippets/MyModule.java
+++ snippets/MyModule.java
@@ -1,7 +1,9 @@
 import com.kalixia.tapestry.rio.services.RioConstants;
+import java.util.concurrent.TimeUnit;
 
 public class MyModule {
     public void contributeApplicationDefaults(MappedConfiguration<String, String> configuration) {
-        configuration.add(RioConstants.DISCOVERY_TIMEOUT, "10");     // discovery times out after 10 seconds
+        configuration.add(RioConstants.DISCOVERY_TIMEOUT, "10");            // discovery times out after 10 ...
+        configuration.add(RioConstants.DISCOVERY_UNIT, TimeUnit.SECONDS);   // ... seconds
     }
 }
